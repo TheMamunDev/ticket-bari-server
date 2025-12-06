2f9fa512e89c0f874f5c5d0531279eb0ac793c06
Mahmud Hasan <mmhasan.mamun2022@gmail.com>
2025-12-07 00:04:48 +0600

    Added featured ticket api  approved by admin with limit

--- controllers/tickets.controller.js
+++ controllers/tickets.controller.js
@@ -6,4 +6,36 @@ const getAllTickets = async (req, res) => {
   res.status(200).send(result);
 };
 
-module.exports = { getAllTickets };
+const getFeaturedTickets = async (req, res) => {
+  try {
+    const result = await ticketsCollection
+      .find({ featured: true })
+      .limit(6)
+      .toArray();
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+const updateMany = async (req, res) => {
+  try {
+    const result = await ticketsCollection.updateMany(
+      { from: 'Dhaka' },
+      {
+        $set: {
+          featured: true,
+        },
+      }
+    );
+
+    res.send({
+      message: 'Updated successfully',
+      result,
+    });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+module.exports = { getAllTickets, updateMany, getFeaturedTickets };
--- routes/tickets.routes.js
+++ routes/tickets.routes.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getAllTickets } = require('../controllers/tickets.controller.js');
+const {
+  getAllTickets,
+  updateMany,
+  getFeaturedTickets,
+} = require('../controllers/tickets.controller.js');
 
 router.get('/', getAllTickets);
+router.put('/', updateMany);
+router.get('/featured', getFeaturedTickets);
 
 module.exports = router;
